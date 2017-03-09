17c79053f45ec2e0c1d298a1481276e19c5a7a9e
Josef Pospíšil <josef.pospisil@topmonks.com>
2017-03-09 21:53:47 +0100

    Add example of effect event and refactor shots (#2)

--- src/cljs/potok_rumu/events.cljs
+++ src/cljs/potok_rumu/events.cljs
@@ -2,26 +2,39 @@
   (:require [potok.core :as ptk]
             [beicon.core :as rx]))
 
-(deftype ^:private SmallShot []
+(deftype ^:private Drink [^Number volume]
   ptk/UpdateEvent
   (update [_ state]
-    (update state :potok (fnil + 0) 0.2)))
+    (update state :potok (fnil + 0) volume)))
+
+(deftype ^:private SmallShot []
+  ptk/WatchEvent
+  (watch [_ _ _]
+    (rx/just (->Drink 0.2))))
 
 (deftype ^:private BigShot []
-  ptk/UpdateEvent
-  (update [_ state]
-    (update state :potok (fnil + 0) 0.5)))
+  ptk/WatchEvent
+  (watch [_ _ _]
+    (rx/just (->Drink 0.5))))
 
 (deftype ^:private Drain []
   ptk/UpdateEvent
   (update [_ state]
     (assoc state :potok 0.0)))
 
+(deftype ^:private Sleep []
+  ptk/EffectEvent
+  (effect [_ _ _]
+    (js/alert "Pššššš, už spí!")))
+
 (deftype ^:private GoHome []
   ptk/WatchEvent
   (watch [_ state stream]
-    (->> (rx/just (->Drain))
-         (rx/delay 2000))))
+    (rx/merge
+     (->> (rx/just (->Drain))
+          (rx/delay 2000))
+     (->> (rx/just(->Sleep))
+          (rx/delay 2500)))))
 
 (def ^:private events-map
   {:small-shot ->SmallShot
