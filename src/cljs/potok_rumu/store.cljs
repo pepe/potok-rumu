75b8c4341d5da84a8766668df6d98482bace38fb
Josef Pospíšil <josef.pospisil@laststar.eu>
2017-04-18 17:35:35 +0200

    Remove unused require

--- src/cljs/potok_rumu/store.cljs
+++ src/cljs/potok_rumu/store.cljs
@@ -1,7 +1,5 @@
 (ns potok-rumu.store
-  (:require [rum.core :as rum]
-            [potok.core :as ptk]))
-
+  (:require [potok.core :as ptk]))
 
 (defonce initial-state
   {:state {:state/potok 0 :state/in-pub? false}})
